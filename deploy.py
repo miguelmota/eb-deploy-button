7f7df0d5e15880c2d2513f413db18f35fcdfb8d4
Miguel Mota <miguelmota2@gmail.com>
2016-10-30 15:31:14 -0700

    get events

--- deploy.py
+++ deploy.py
@@ -3,6 +3,8 @@ import json
 
 client = boto3.client('elasticbeanstalk')
 
+env = 'reportal-docker-dev'
+
 def deployLatestVersion():
     response = client.describe_application_versions(
         ApplicationName='reportal',
@@ -12,8 +14,42 @@ def deployLatestVersion():
     latestVersion = response['ApplicationVersions'][0]['VersionLabel']
 
     response = client.update_environment(
-        EnvironmentName='reportal-docker-dev',
+        EnvironmentName=env,
         VersionLabel=latestVersion
     )
 
     print(response)
+    return response
+
+def getEnvEvents():
+    response = client.describe_events(
+        EnvironmentName=env,
+        MaxRecords=25
+    )
+
+    for event in response['Events']:
+        message = event['Message']
+        eventType = event['Severity']
+        date = event['EventDate']
+        print(message)
+
+    return response
+
+def getEnvHealth():
+    response = client.describe_environment_health(
+        AttributeNames=[
+            'All'
+        ],
+        EnvironmentName=env,
+    )
+
+    color = response['Color']
+    status = response['Status']
+    health_status = response['HealthStatus']
+    refreshed_at = response['RefreshedAt']
+
+    print(health_status)
+    return response
+
+if __name__ == '__main__':
+    getEnvHealth();
