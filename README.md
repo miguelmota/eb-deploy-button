0e9ff7a5ad9ae9d88e0df4ce77630a17c40b3fee
Miguel Mota <miguelmota2@gmail.com>
2016-10-31 03:06:10 +0000

    deploy script

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Portal Deploy Button
 
 <img src="./assets/big_red_button.jpg" width="400">
+
+# Run
+
+```bash
+python deploy.py
+```
--- deploy.py
+++ deploy.py
@@ -1,9 +1,63 @@
 import boto3
+import time
 import json
+from termcolor import colored
+from big_red import BigRedButton
 
 client = boto3.client('elasticbeanstalk')
 
 env = 'reportal-docker-dev'
+
+class BoringButton(BigRedButton):
+    def on_unknown(self):
+	      print(colored('The button is in an unknown state.\n', 'yellow'))
+
+    def on_cover_open(self):
+        print(colored('WARNING!! The cover has been opened. DEPLOY AT YOUR RISK.\n', 'yellow', attrs=['bold']))
+
+    def on_cover_close(self):
+        print(colored('The cover has been closed.\n', 'white'))
+
+    def on_button_press(self):
+        print(colored('The button has been pressed. Release to deploy.\n', 'yellow'))
+
+    def on_button_release(self):
+        print(colored('---DEPLOY INITIATED---\n', 'green', attrs=['bold']))
+        print(colored('Please close lid.\n', 'white'))
+        deployLatestVersion()
+        time.sleep(10)
+        health_status = None
+        message = None
+
+        while not (health_status == 'Ok' or health_status == 'Severe'):
+          time.sleep(5)
+          last_health_status = health_status
+          health_response = getEnvHealth()
+          color = health_response['Color'].lower()
+          status = health_response['Status']
+          health_status = health_response['HealthStatus']
+          refreshed_at = health_response['RefreshedAt']
+
+          health_status_c = colored(health_status, color);
+
+          if last_health_status != health_status:
+            print('Health status: ' + health_status_c + '\n')
+
+          env_events_response = getEnvEvents()
+
+          for event in env_events_response['Events']:
+              last_message = message
+              message = event['Message']
+              event_type = event['Severity']
+              date = event['EventDate']
+              if last_message != message:
+                print(colored('> ', 'blue') + colored(message, 'white') + '\n')
+
+        if health_status == 'Ok':
+          print(colored('Deploy successful.\n', 'green', attrs=['bold']))
+        else:
+          print(colored('There was an error with deployment.\n', 'red', attrs=['bold']))
+
 
 def deployLatestVersion():
     response = client.describe_application_versions(
@@ -11,27 +65,21 @@ def deployLatestVersion():
         MaxRecords=1
     )
 
-    latestVersion = response['ApplicationVersions'][0]['VersionLabel']
+    latest_version = response['ApplicationVersions'][0]['VersionLabel']
 
     response = client.update_environment(
         EnvironmentName=env,
-        VersionLabel=latestVersion
+        VersionLabel=latest_version
     )
 
-    print(response)
+    print(colored('Deploying latest version: ' + latest_version + '\n', 'white', attrs=['bold']))
     return response
 
-def getEnvEvents():
+def getEnvEvents(max_records=1):
     response = client.describe_events(
         EnvironmentName=env,
-        MaxRecords=25
+        MaxRecords=max_records
     )
-
-    for event in response['Events']:
-        message = event['Message']
-        eventType = event['Severity']
-        date = event['EventDate']
-        print(message)
 
     return response
 
@@ -43,13 +91,9 @@ def getEnvHealth():
         EnvironmentName=env,
     )
 
-    color = response['Color']
-    status = response['Status']
-    health_status = response['HealthStatus']
-    refreshed_at = response['RefreshedAt']
-
-    print(health_status)
     return response
 
 if __name__ == '__main__':
-    getEnvHealth();
+    print(colored('On stand by..\n', 'white'))
+    button = BoringButton()
+    button.run()
